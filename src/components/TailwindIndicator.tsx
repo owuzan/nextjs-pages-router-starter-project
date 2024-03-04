81846f3344fb733db1df161493dbc279f0b7a6b2
Oğuzhan Yılmaz <ooguzhanyilmazz41@gmail.com>
2024-03-04 18:02:37 +0300

    Refactor TailwindIndicator component

--- src/components/TailwindIndicator.tsx
+++ src/components/TailwindIndicator.tsx
@@ -8,13 +8,15 @@ const TailwindIndicator: FC<TailwindIndicatorProps> = (props) => {
     return null;
   }
   return (
-    <div className="fixed bottom-10 left-0 right-0 flex items-center justify-center gap-2 font-medium opacity-50 pointer-events-none">
-      <strong>Screen:</strong>
-      <span className="sm:hidden">DEFAULT</span>
-      <span className="hidden sm:block md:hidden">SM</span>
-      <span className="hidden md:block lg:hidden">MD</span>
-      <span className="hidden lg:block xl:hidden">XL</span>
-      <span className="hidden xl:block">2XL</span>
+    <div className="fixed bottom-10 left-0 right-0 flex justify-center pointer-events-none opacity-50">
+      <p className="inline-flex items-center gap-2 bg-zinc-100 rounded px-2 py-0.5 border border-zinc-200 font-medium text-sm">
+        <strong>Screen:</strong>
+        <span className="sm:hidden">DEFAULT</span>
+        <span className="hidden sm:block md:hidden">SM</span>
+        <span className="hidden md:block lg:hidden">MD</span>
+        <span className="hidden lg:block xl:hidden">XL</span>
+        <span className="hidden xl:block">2XL</span>
+      </p>
     </div>
   );
 };
